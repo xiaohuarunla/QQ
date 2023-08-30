5ffb37361338af81f1f010de14c24063498276a6
xiaoxiaorun <lilyligffa@gmail.com>
2023-08-30 11:48:21 +0800

    测试
    Signed-off-by: xiaoxiaorun <lilyligffa@gmail.com>

--- Mainwindow.cpp
+++ Mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "Mainwindow.h"
 #include "ui_Mainwindow.h"
-
+#include<QDebug>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug()<<"aaaaa";
 }
 
 MainWindow::~MainWindow()
